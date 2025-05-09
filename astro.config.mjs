0dc7d78a64d3d030c5aa56a6636b03226751a7df
Matthew Segal <matt.segal@themobian.com>
2025-05-09 10:21:08 +1000

    Deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mattsegal.github.io',
+  base: '/time-estimation',
   outDir: './docs',
   integrations: [react()]
 });
